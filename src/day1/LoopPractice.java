d0a4bf1070e526bec5df022f7221c63da5969b00
beibitbaimuratov <bbaimurat@gmail.com>
2020-02-29 23:45:14 -0500

    add "world"

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -7,7 +7,7 @@ public class LoopPractice {
         for (int i = 0; i < 10 ; i++) {
             System.out.println("Hello");
 
-            System.out.println("####");
+            System.out.println("####hhhh");
 
             System.out.println("World");
         }
